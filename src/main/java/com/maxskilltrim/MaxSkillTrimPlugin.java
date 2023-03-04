a4d936ee148fdcca35992ddb0b94717bf170f146
Nathan Quayle <nathan.quayle@gmail.com>
2023-03-04 21:58:12 +0000

    Fixes #2.

--- src/main/java/com/maxskilltrim/MaxSkillTrimPlugin.java
+++ src/main/java/com/maxskilltrim/MaxSkillTrimPlugin.java
@@ -2,10 +2,17 @@ package com.maxskilltrim;
 
 import javax.imageio.ImageIO;
 import javax.inject.Inject;
-
 import com.google.inject.Provides;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
+import net.runelite.api.Experience;
+import net.runelite.api.GameState;
+import net.runelite.api.SpritePixels;
+import net.runelite.api.events.ScriptPostFired;
+import net.runelite.api.events.ScriptPreFired;
+import net.runelite.api.widgets.*;
 import net.runelite.client.RuneLite;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.ConfigChanged;
@@ -13,8 +20,7 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.ClientToolbar;
 import net.runelite.client.ui.NavigationButton;
-import net.runelite.client.ui.overlay.OverlayManager;
-
+import net.runelite.client.util.ImageUtil;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -29,11 +35,7 @@ import java.util.Objects;
 )
 public class MaxSkillTrimPlugin extends Plugin
 {
-    public BufferedImage currentTrimImage;
-    @Inject
-    private MaxSkillTrimOverlay overlay;
-    @Inject
-    private OverlayManager overlayManager;
+    private static final int SPRITE_ID = -666;
     private MaxSkillTrimPanel maxSkillTrimPanel;
     @Inject
     private MaxSkillTrimConfig maxSkillTrimConfig;
@@ -43,6 +45,13 @@ public class MaxSkillTrimPlugin extends Plugin
     @Inject
     private ConfigManager configManager;
     public static final File MAXSKILLTRIMS_DIR = new File(RuneLite.RUNELITE_DIR.getPath() + File.separator + "max-skill-trims");
+    private static final int SCRIPTID_STATS_INIT = 393;
+    @Inject
+    private Client client;
+    @Inject
+    private ClientThread clientThread;
+    private Widget currentWidget;
+    private Widget[] trims = new Widget[SkillData.values().length];
 
     @Provides
     MaxSkillTrimConfig provideConfig(ConfigManager configManager)
@@ -60,15 +69,12 @@ public class MaxSkillTrimPlugin extends Plugin
 
         addDefaultTrims();
 
-        overlayManager.add(overlay);
-
         maxSkillTrimPanel = injector.getInstance(MaxSkillTrimPanel.class);
 
         BufferedImage icon;
         synchronized (ImageIO.class)
         {
             icon = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/icon.png")));
-            currentTrimImage = ImageIO.read(new File(MAXSKILLTRIMS_DIR, maxSkillTrimConfig.selectedMaxSkillTrimFilename()));
         }
 
         navButton = NavigationButton.builder()
@@ -79,6 +85,93 @@ public class MaxSkillTrimPlugin extends Plugin
                 .build();
 
         pluginToolbar.addNavigation(navButton);
+
+        overrideSprites();
+
+        if (client.getGameState() == GameState.LOGGED_IN) {
+            clientThread.invoke(this::buildSkillBars);
+        }
+    }
+
+    @Subscribe
+    public void onScriptPreFired(ScriptPreFired event) {
+        if (event.getScriptId() != SCRIPTID_STATS_INIT)	{
+            return;
+        }
+        currentWidget = event.getScriptEvent().getSource();
+    }
+
+    @Subscribe
+    public void onScriptPostFired(ScriptPostFired event) {
+        if (event.getScriptId() == SCRIPTID_STATS_INIT && currentWidget != null) {
+            buildSkillBar(currentWidget);
+        }
+    }
+
+    private void buildSkillBars() {
+        Widget skillsContainer = client.getWidget(WidgetInfo.SKILLS_CONTAINER);
+        if (skillsContainer == null) {
+            return;
+        }
+
+        for (Widget skillTile : skillsContainer.getStaticChildren()) {
+            buildSkillBar(skillTile);
+        }
+    }
+
+    private void removeSkillBars() {
+        for ( Widget trim : trims ) {
+            if (trim == null) {
+                continue;
+            }
+
+            Widget[] children = trim.getParent().getChildren();
+            for (int i = 0; i < children.length; i++) {
+                if (trim == children[i]) {
+                    children[i] = null;
+                }
+            }
+        }
+        trims = new Widget[SkillData.values().length];
+    }
+
+
+    /**
+     * Create the widgets needed for the bars to exist, and keep a reference to them
+     *
+     * @param parent The parent widget inside which the skill bar is created
+     */
+    private void buildSkillBar(Widget parent) {
+        int idx = WidgetInfo.TO_CHILD(parent.getId()) - 1;
+        SkillData skill = SkillData.get(idx);
+        if (skill == null) {
+            return;
+        }
+
+        Widget trim = parent.createChild(-1, WidgetType.GRAPHIC);
+        trim.setYPositionMode(WidgetPositionMode.ABSOLUTE_TOP);
+        trim.setOriginalHeight(parent.getOriginalHeight());
+        trim.setOriginalWidth(parent.getOriginalWidth());
+        trim.setWidthMode(parent.getWidthMode());
+        trim.setHeightMode(parent.getHeightMode());
+        trim.setOpacity(255);
+        trim.setSpriteId(SPRITE_ID);
+        JavaScriptCallback cb = ev -> updateTrim(skill, trim);
+
+        trim.setHasListener(true);
+        trim.setOnVarTransmitListener(cb);
+
+        trim.revalidate();
+        trims[idx] = trim;
+    }
+
+    private void updateTrim(SkillData skill, Widget trim) {
+        final int currentXP = client.getSkillExperience(skill.getSkill());
+        final int currentLevel = Experience.getLevelForXp(currentXP);
+
+        if(currentLevel >= Experience.MAX_REAL_LEVEL) {
+            trim.setOpacity(0);
+        }
     }
 
     private void addDefaultTrims()
@@ -96,8 +189,8 @@ public class MaxSkillTrimPlugin extends Plugin
     @Override
     protected void shutDown() throws Exception
     {
-        overlayManager.remove(overlay);
         pluginToolbar.removeNavigation(navButton);
+        clientThread.invoke(this::removeSkillBars);
     }
 
     @Subscribe
@@ -107,17 +200,7 @@ public class MaxSkillTrimPlugin extends Plugin
         {
             switch(event.getKey()) {
                 case MaxSkillTrimConfig.SELECTED_MAX_SKILL_TRIM:
-                    try
-                    {
-                        synchronized (ImageIO.class)
-                        {
-                            currentTrimImage = ImageIO.read(new File(MAXSKILLTRIMS_DIR, event.getNewValue()));
-                        }
-                    }
-                    catch (IOException ioException)
-                    {
-                        log.warn(null, ioException);
-                    }
+                    overrideSprites();
                     break;
                 case MaxSkillTrimConfig.SHOW_NAV_BUTTON:
                     boolean showNavButton = Boolean.TRUE.toString().equals(event.getNewValue());
@@ -126,8 +209,42 @@ public class MaxSkillTrimPlugin extends Plugin
                     if(!showNavButton) pluginToolbar.removeNavigation(navButton);
                     break;
             }
-
         }
+    }
+
+    public SpritePixels getSpritePixels()
+    {
+        File spriteFile = new File(MAXSKILLTRIMS_DIR + File.separator + maxSkillTrimConfig.selectedMaxSkillTrimFilename());
+        if (!spriteFile.exists())
+        {
+            log.debug("Sprite doesn't exist (" + spriteFile.getPath() + "): ");
+            return null;
+        }
+        try
+        {
+            BufferedImage image = ImageIO.read(spriteFile);
+
+            return ImageUtil.getImageSpritePixels(image, client);
+        }
+        catch (RuntimeException | IOException ex)
+        {
+            log.debug("Unable to find image (" + spriteFile.getPath() + "): ");
+        }
+        return null;
+    }
+
+    void overrideSprites()
+    {
+        SpritePixels spritePixels = getSpritePixels();
+
+        if (spritePixels == null)
+        {
+            return;
+        }
+
+        client.getSpriteOverrides().remove(SPRITE_ID);
+        client.getWidgetSpriteCache().reset();
+        client.getSpriteOverrides().put(SPRITE_ID, spritePixels);
     }
 
     public void setImage(MaxSkillTrimFile file)
