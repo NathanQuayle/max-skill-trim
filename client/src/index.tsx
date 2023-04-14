db194a3454470d2d5c670d8269ba49d446c19313
Nathan Quayle <nathan.quayle@gmail.com>
2023-04-14 21:15:30 +0100

    Moves theme.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,18 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { MantineProvider } from '@mantine/core'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <MantineProvider
-      theme={{ colorScheme: 'dark' }}
-      withGlobalStyles
-      withNormalizeCSS
-    >
-      <App />
-    </MantineProvider>
+    <App />
   </React.StrictMode>
 )
 
